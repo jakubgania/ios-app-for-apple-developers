79227b3044e944ce7a9e0d18d04929d341cadb07
Jakub Gania <contact@jakubgania.io>
2022-11-06 14:30:08 +0100

    Initial Commit

---
+++ ForAppleDevelopers/ForAppleDevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForAppleDevelopersApp.swift
+//  ForAppleDevelopers
+//
+//  Created by Jakub Gania on 06.11.22.
+//
+
+import SwiftUI
+
+@main
+struct ForAppleDevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
